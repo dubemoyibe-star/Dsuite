b83d43c8f8fb9baa2c42e953c605b73b98bf225c
dubemoyibe-star <dubemoyibe@gmail.com>
2026-01-04 11:05:30 +0100

    completely implemented navigation in the homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { FaBed, FaSnowflake } from "react-icons/fa";
 import { SlCalender } from "react-icons/sl";
 import { FaHeadphones } from "react-icons/fa6";
@@ -16,8 +17,10 @@ export default function Home() {
           <h1 className="text-5xl font-medium mb-4 text-yellow-700 text-shadow-lg font-serif sm:text-6xl">Book Comfortable <span className='block'>Suites With Ease</span></h1>
           <p className="text-gray-200 font-serif text-shadow-sm text-shadow-black font-medium text-shadow-lg text-2xl sm:text-3xl">Modern Rooms, Simple Bookings, <span className='block'>Travel Services</span></p>
           <div>
-            <button className="mt-6 px-8 py-2 bg-linear-to-r from-yellow-600 to-yellow-800 text-white font-semibold rounded-lg hover:shadow-2xl active:shadow-2xl cursor-pointer hover:bg-linear-to-l from-yellow-600 to-yellow-800 active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300">Book Now</button>
-            <button className="mt-6 ml-4 px-8 py-2 bg-white border-solid border-2 border-yellow-600 text-gray-800 font-semibold rounded-lg hover:bg-linear-to-r from-yellow-600 to-yellow-800 active:bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer hover:text-white active:text-white transition-colors duration-300">Learn More</button>
+            <Link to="rooms"><button className="mt-6 px-8 py-2 bg-linear-to-r from-yellow-600 to-yellow-800 text-white font-semibold 
+            rounded-lg hover:shadow-2xl active:shadow-2xl cursor-pointer hover:bg-linear-to-l from-yellow-600 to-yellow-800 active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300">Book Now</button></Link>
+           <Link to="about"><button className="mt-6 ml-4 px-8 py-2 bg-white border-solid border-2 border-yellow-600 text-gray-800 font-semibold 
+            rounded-lg hover:bg-linear-to-r from-yellow-600 to-yellow-800 active:bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer hover:text-white active:text-white transition-colors duration-300">Learn More</button></Link> 
           </div>
         </section>
 
@@ -55,7 +58,7 @@ export default function Home() {
               <div className='p-6 flex flex-col '>
                 <h3 className='text-2xl font-semibold font-serif'>Exclusive Suite</h3>
                 <p className='mt-2  text-gray-700 font-medium text-lg '>starting at $250 /night</p>
-                <button className=" mt-4 px-6 py-2 text-white font-semibold font-serif text-lg rounded-lg hover:bg-linear-to-l from-yellow-600 to-yellow-800  hover:shadow-2xl bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300">View Details</button>
+                <Link to="rooms/1" className="text-center mt-4 px-6 py-2 text-white font-semibold font-serif text-lg rounded-lg hover:bg-linear-to-l from-yellow-600 to-yellow-800  hover:shadow-2xl bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300"><button>View Details</button></Link>
               </div>
             </div> 
           
@@ -67,7 +70,7 @@ export default function Home() {
               <div className='p-6 flex flex-col '>
                 <h3 className='text-2xl font-semibold font-serif'>Regular Suite</h3>
                 <p className='mt-2  text-gray-700 font-medium text-lg '>starting at $150 /night</p>
-                <button className=" mt-4 px-6 py-2 text-white font-semibold font-serif text-lg rounded-lg hover:bg-linear-to-l from-yellow-600 to-yellow-800  hover:shadow-2xl bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300">View Details</button>
+                <Link to="rooms/2" className="text-center mt-4 px-6 py-2 text-white font-semibold font-serif text-lg rounded-lg hover:bg-linear-to-l from-yellow-600 to-yellow-800  hover:shadow-2xl bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300"><button>View Details</button></Link>
               </div>
             </div> 
   
@@ -79,7 +82,7 @@ export default function Home() {
               <div className='p-6 flex flex-col '>
                 <h3 className='text-2xl font-semibold font-serif'>Family Suite</h3>
                 <p className='mt-2  text-gray-700 font-medium text-lg '>starting at $300 /night</p>
-                <button className=" mt-4 px-6 py-2 text-white font-semibold font-serif text-lg rounded-lg hover:bg-linear-to-l from-yellow-600 to-yellow-800  hover:shadow-2xl bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300">View Details</button>
+               <Link to="rooms/3" className="text-center mt-4 px-6 py-2 text-white font-semibold font-serif text-lg rounded-lg hover:bg-linear-to-l from-yellow-600 to-yellow-800  hover:shadow-2xl bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300"><button>View Details</button></Link>
               </div>
             </div> 
           </div>
@@ -127,8 +130,10 @@ export default function Home() {
           <h3 className='mb-4 text-medium text-3xl sm:text-4xl text-yellow-700 font-serif text-center '>Ready to Book Your Stay?</h3>
           <p className='mt-2 mb-8 text-lg font-medium '>Experience the best in comfort and convenience with DSuite. Book your suite today!</p>
           <div className='flex justify-center flex-col md:flex-row gap-4'>
-            <button className="px-10 py-2 bg-linear-to-r from-yellow-600 to-yellow-800 text-white font-semibold rounded-lg hover:shadow-2xl cursor-pointer hover:bg-linear-to-l from-yellow-600 to-yellow-800 active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300">Book Now</button>
-            <button className="px-10 py-2 bg-white border-solid border-2 border-yellow-600 text-gray-800 font-semibold rounded-lg hover:bg-linear-to-r from-yellow-600 to-yellow-800 active:bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer hover:text-white active:text-white transition-colors duration-300">Contact Us</button>
+            <Link to="rooms" className="px-10 py-2 bg-linear-to-r from-yellow-600 to-yellow-800 text-white font-semibold rounded-lg hover:shadow-2xl 
+            cursor-pointer hover:bg-linear-to-l from-yellow-600 to-yellow-800 active:bg-linear-to-l from-yellow-600 to-yellow-800 transition-colors duration-300"><button>Book Now</button></Link>
+            <Link to="contact-us" className="px-10 py-2 bg-white border-solid border-2 border-yellow-600 text-gray-800 font-semibold rounded-lg 
+            hover:bg-linear-to-r from-yellow-600 to-yellow-800 active:bg-linear-to-r from-yellow-600 to-yellow-800 cursor-pointer hover:text-white active:text-white transition-colors duration-300"><button>Contact Us</button></Link>
           </div>
         </section>
       </main>
