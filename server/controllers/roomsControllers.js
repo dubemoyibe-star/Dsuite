f0d5de993e559f12d5841d2d00c1781f0fd61a37
dubemoyibe-star <dubemoyibe@gmail.com>
2026-01-08 10:07:37 +0100

    some production config

--- server/controllers/roomsControllers.js
+++ server/controllers/roomsControllers.js
@@ -1,10 +1,11 @@
 import { getDbConnection  } from "../db.js";
 
 export async function getRooms(req, res) {
- 
+ console.log("Rooms route hit");
  try {
    const db = await getDbConnection()
    const rooms = await db.all('SELECT * FROM rooms');
+   console.log("DB opened");
    res.status(200).json(rooms);
  } catch (error) {
    res.status(500).json({ error: 'Failed to fetch rooms' });
--- server/db.js
+++ server/db.js
@@ -1,15 +1,21 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-import path from 'node:path'
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import path from "node:path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const dbPath =
+  process.env.NODE_ENV === "production"
+    ? process.env.DATABASE_PATH 
+    : path.join(__dirname, "./database.db"); 
+
 export async function getDbConnection() {
   const db = await open({
-    filename: path.join(__dirname, './database.db'), 
-    driver: sqlite3.Database
+    filename: dbPath,
+    driver: sqlite3.Database,
   });
+
   return db;
 }
