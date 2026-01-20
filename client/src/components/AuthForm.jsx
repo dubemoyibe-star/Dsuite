99805f938482940877e51d439fbfe31e569798b7
dubemoyibe-star <dubemoyibe@gmail.com>
2026-01-20 17:24:50 +0100

    implemented password toggle

--- client/src/components/AuthForm.jsx
+++ client/src/components/AuthForm.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { AuthContext } from "../contexts/AuthContext";
-import { useLocation, useNavigate } from "react-router-dom"; 
+import { useLocation, useNavigate } from "react-router-dom";
+import { IoEyeOff , IoEye } from "react-icons/io5"; 
 
 
 export default function AuthForm({ mode }) {
@@ -24,6 +25,7 @@ export default function AuthForm({ mode }) {
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState("");
   const [success, setSuccess] = React.useState(false);
+  const [showPassword, setShowPassword] = React.useState(false);
   
   React.useEffect(() => {
   setFormData(formDataInitialState);
@@ -85,21 +87,23 @@ export default function AuthForm({ mode }) {
   };
 
   return (
-    <section className={`${!isSignUp && "h-screen"} bg-gray-200 sm:py-4 bg-[linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.75)),url('/assets/auth-bg.jpg')] bg-cover bg-center min-h-screen `}>
+    <section className={`h-screen bg-gray-200 sm:py-4 bg-[linear-gradient(rgba(0,0,0,0.6),rgba(0,0,0,0.75)),url('/assets/auth-bg.jpg')] bg-cover bg-center min-h-screen `}>
       <form
         onSubmit={handleSubmit}
         className="bg-gray-50 sm:rounded-2xl shadow-xl p-6 lg:p-8 flex flex-col gap-4 max-w-lg mx-auto h-full"
       >
-        <Link to="/" className="flex justify-center mb-2">
-          <div className="h-18 w-18 flex items-center justify-center rounded-full bg-yellow-100 shadow-sm shadow-gray-200">
-            <img src={logo} alt="Logo" className="w-full h-full object-contain" />
+        
+          <div className="flex justify-center mb-2">
+            <Link to="/" className="h-18 w-18 flex items-center justify-center rounded-full bg-yellow-100 shadow-sm shadow-gray-200">
+              <img src={logo} alt="Logo" className="w-full h-full object-contain" />
+            </Link>
           </div>
-        </Link>
+       
 
-        <h2 className="text-2xl lg:text-3xl font-serif font-bold text-center text-gray-800">
+        <h2 className="text-2xl lg:text-3xl font-serif font-bold text-center text-gray-800 mb-[-6]">
           { isSignUp ? "Create Your Account" : "Welcome Back" }
         </h2>
-        <p className="text-center text-sm text-gray-500 mb-2">
+        <p className="text-center text-gray-500 mb-2">
          {isSignUp ? "Join us and enjoy a refined luxury experience" : "Log in to your account."}
         </p>
 
@@ -161,20 +165,31 @@ export default function AuthForm({ mode }) {
           />
         </div> : null}
 
-        <div className="flex flex-col gap-1">
-          <label htmlFor="password" className="text-sm  font-medium text-gray-700">
-            Password
-          </label>
+       <div className="flex flex-col gap-1">
+        <label htmlFor="password" className="text-sm font-medium text-gray-700">
+          Password
+        </label>
+
+        <div className="relative">
           <input
             id="password"
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="••••••••"
             value={formData.password}
             onChange={handleChange}
             required
-            className="border rounded-md px-4 py-2 focus:outline-none focus:ring-1 focus:ring-yellow-700"
+            className="border rounded-md px-4 py-2 pr-10 w-full focus:outline-none focus:ring-1 focus:ring-yellow-700"
           />
+
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            >
+              {showPassword ? <IoEye className="w-8 text-xl text-yellow-700 cursor-pointer"/> : <IoEyeOff className="w-8 text-xl text-yellow-700 cursor-pointer"/>}
+            </button>
+          </div>
         </div>
 
         <button
@@ -184,7 +199,7 @@ export default function AuthForm({ mode }) {
         >
           {loading ? (isSignUp ? "Creating account..." : "Logging in...") : (isSignUp  ? "Create Account" : "Log In")}
         </button>
-        <p className="text-center text-sm text-gray-600 mt-pt">
+        <p className="text-center text-sm text-gray-600 pb-4">
           {isSignUp ? "Already have an account?" : "Don't have an account yet?"}
           <Link to={isSignUp ? "/login" : "/signup"} className="text-yellow-700 font-medium hover:underline">
            {isSignUp ? " Log in" : " Sign up"}
