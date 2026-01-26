878da294cab7dc996932114c8e437728798589b8
dubemoyibe-star <dubemoyibe@gmail.com>
2026-01-26 17:08:15 +0100

    made important seo optimisation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { FaBed, FaSnowflake } from "react-icons/fa";
 import { SlCalender } from "react-icons/sl";
--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from 'react-router-dom'
 import { FaBed } from "react-icons/fa";
 import { motion } from "framer-motion";
